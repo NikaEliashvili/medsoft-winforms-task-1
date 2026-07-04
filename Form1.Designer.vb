0f6dade18122f3503736b11e35314f253e469fae
NikaEliashvili <nikaeliashvili18@outlook.com>
2026-07-04 10:19:53 +0400

    Initial commit: Patients CRUD application base setup

---
+++ Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.btnEdit = New DevExpress.XtraEditors.SimpleButton()
+        Me.btnAdd = New DevExpress.XtraEditors.SimpleButton()
+        Me.btnDelete = New DevExpress.XtraEditors.SimpleButton()
+        Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
+        Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.colID = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.colFullName = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.colDob = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.colGender = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.colPhone = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.colAddress = New DevExpress.XtraGrid.Columns.GridColumn()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.btnEdit)
+        Me.PanelControl1.Controls.Add(Me.btnAdd)
+        Me.PanelControl1.Controls.Add(Me.btnDelete)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(1181, 68)
+        Me.PanelControl1.TabIndex = 0
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Appearance.Font = New System.Drawing.Font("Tahoma", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEdit.Appearance.Options.UseFont = True
+        Me.btnEdit.ImageOptions.Image = Global.patients_crud_app.My.Resources.Resources.edit
+        Me.btnEdit.Location = New System.Drawing.Point(139, 0)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(181, 68)
+        Me.btnEdit.TabIndex = 1
+        Me.btnEdit.Text = "რედაქტირება"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Appearance.Font = New System.Drawing.Font("Tahoma", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Appearance.Options.UseFont = True
+        Me.btnAdd.ImageOptions.Image = Global.patients_crud_app.My.Resources.Resources.add
+        Me.btnAdd.Location = New System.Drawing.Point(0, 0)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(143, 68)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "დამატება"
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Appearance.BackColor = System.Drawing.Color.Transparent
+        Me.btnDelete.Appearance.BackColor2 = System.Drawing.Color.Transparent
+        Me.btnDelete.Appearance.BorderColor = System.Drawing.Color.Transparent
+        Me.btnDelete.Appearance.Font = New System.Drawing.Font("Tahoma", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Appearance.ForeColor = System.Drawing.Color.Black
+        Me.btnDelete.Appearance.Options.UseBackColor = True
+        Me.btnDelete.Appearance.Options.UseBorderColor = True
+        Me.btnDelete.Appearance.Options.UseFont = True
+        Me.btnDelete.Appearance.Options.UseForeColor = True
+        Me.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnDelete.ImageOptions.Image = Global.patients_crud_app.My.Resources.Resources.delete
+        Me.btnDelete.Location = New System.Drawing.Point(320, 0)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(122, 68)
+        Me.btnDelete.TabIndex = 2
+        Me.btnDelete.Text = "წაშლა"
+        '
+        'GridControl1
+        '
+        Me.GridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GridControl1.Location = New System.Drawing.Point(0, 68)
+        Me.GridControl1.MainView = Me.GridView1
+        Me.GridControl1.Name = "GridControl1"
+        Me.GridControl1.Size = New System.Drawing.Size(1181, 588)
+        Me.GridControl1.TabIndex = 1
+        Me.GridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
+        '
+        'GridView1
+        '
+        Me.GridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.colID, Me.colFullName, Me.colDob, Me.colGender, Me.colPhone, Me.colAddress})
+        Me.GridView1.GridControl = Me.GridControl1
+        Me.GridView1.Name = "GridView1"
+        '
+        'colID
+        '
+        Me.colID.Caption = "ID"
+        Me.colID.FieldName = "ID"
+        Me.colID.MinWidth = 30
+        Me.colID.Name = "colID"
+        Me.colID.Visible = True
+        Me.colID.VisibleIndex = 0
+        Me.colID.Width = 112
+        '
+        'colFullName
+        '
+        Me.colFullName.Caption = "პაციენტის გვარი, სახელი"
+        Me.colFullName.FieldName = "FullName"
+        Me.colFullName.MinWidth = 30
+        Me.colFullName.Name = "colFullName"
+        Me.colFullName.Visible = True
+        Me.colFullName.VisibleIndex = 1
+        Me.colFullName.Width = 112
+        '
+        'colDob
+        '
+        Me.colDob.Caption = "დაბ. თარიღი"
+        Me.colDob.FieldName = "Dob"
+        Me.colDob.MinWidth = 30
+        Me.colDob.Name = "colDob"
+        Me.colDob.Visible = True
+        Me.colDob.VisibleIndex = 2
+        Me.colDob.Width = 112
+        '
+        'colGender
+        '
+        Me.colGender.Caption = "სქესი"
+        Me.colGender.FieldName = "Gender"
+        Me.colGender.MinWidth = 30
+        Me.colGender.Name = "colGender"
+        Me.colGender.Visible = True
+        Me.colGender.VisibleIndex = 3
+        Me.colGender.Width = 112
+        '
+        'colPhone
+        '
+        Me.colPhone.Caption = "მობ. ნომერი"
+        Me.colPhone.FieldName = "Phone"
+        Me.colPhone.MinWidth = 30
+        Me.colPhone.Name = "colPhone"
+        Me.colPhone.Visible = True
+        Me.colPhone.VisibleIndex = 4
+        Me.colPhone.Width = 112
+        '
+        'colAddress
+        '
+        Me.colAddress.Caption = "მისამართი"
+        Me.colAddress.FieldName = "Address"
+        Me.colAddress.MinWidth = 30
+        Me.colAddress.Name = "colAddress"
+        Me.colAddress.Visible = True
+        Me.colAddress.VisibleIndex = 5
+        Me.colAddress.Width = 112
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1181, 656)
+        Me.Controls.Add(Me.GridControl1)
+        Me.Controls.Add(Me.PanelControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents btnAdd As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents btnDelete As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents btnEdit As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents colID As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents colFullName As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents colDob As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents colGender As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents colPhone As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents colAddress As DevExpress.XtraGrid.Columns.GridColumn
+End Class
---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub GridControl1_Click(sender As Object, e As EventArgs) Handles GridControl1.Click
+
+    End Sub
+
+    Private Sub PanelControl1_Paint(sender As Object, e As PaintEventArgs) Handles PanelControl1.Paint
+
+    End Sub
+End Class
